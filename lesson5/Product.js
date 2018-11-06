3eae18e610920b5ffc82122c3825de51494b9d89
Maxim Eliseev <35292056+eliseevma@users.noreply.github.com>
2018-11-06 16:20:31 +0300

    5

---
+++ lesson5/Product.js
@@ -0,0 +1,48 @@
+class Product {
+    constructor(id, title, price, img = 'http://placehold.it/200x150', container = '#products') {
+        this.id = id;
+        this.price = price;
+        this.title = title;
+        this.img = img;
+        this.container = container;
+        this._render(this.container);
+    }
+
+    _render(container) {
+        let $wrapper = $('<div/>',{
+            class: 'product'
+        });
+
+        let $img = $('<img/>',{
+            src: this.img
+        });
+
+        let $desc = $('<div/>',{
+            class: 'desc'
+        });
+
+        let $name = $('<p/>',{
+            text: this.title
+        });
+
+        let $price = $(`<p> Цена: <span class="product-price">${this.price}</span></p>`,);
+
+        let $buyBtn = $('</button>', {
+            class: 'buyBtn',
+            text: 'Купить',
+            'data-id': this.id,
+            'data-name': this.title,
+            'data-price':this.price
+        });
+
+        //Собираем структуру
+        $img.appendTo($wrapper);
+        $name.appendTo($desc);
+        $price.appendTo($desc);
+        $buyBtn.appendTo($desc);
+        $desc.appendTo($wrapper);
+
+        $(container).append($wrapper);
+
+    }
+}
